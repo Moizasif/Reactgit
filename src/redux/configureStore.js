2ba9212f18c4cb012b594bc48c1ed5ef852e7374
Moiz Asif <kmoizasif019@gmail.com>
2020-07-07 15:30:56 +0500

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,26 @@
-import { createStore} from 'redux';
-import {Reducer ,initialState} from './reducer';
+import { createStore , combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 
 
 /*TO configure a store we need to create a store 
-This createStore take two parameters*/ 
+This createStore take two parameters
+Combine reducer is a method redux provides to combine all the reducers that we split*/ 
+
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState,
+        combineReducers({
 
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+
+        })
+   
     );
     return store;
 }
