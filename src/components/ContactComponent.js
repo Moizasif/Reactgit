22ee1084b55cb31bdc8533e7e74acad3142495a1
Moiz Asif <kmoizasif019@gmail.com>
2020-07-16 14:14:08 +0500

    React Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button, Label, Input, Col, Row} from 'reactstrap';
 import {Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 /*Function define for validations*/
 
@@ -27,6 +27,7 @@ and the validate also not require*/
     handleSubmit(values){
         console.log("Current state is: "+ JSON.stringify(values));
         alert("Current state is: "+ JSON.stringify(values));
+        this.props.resetFeedbackForm();
       
     }
 
@@ -80,7 +81,7 @@ and the validate also not require*/
                      <h3>Send us Your Feedback</h3>
                    </div>
                    <div className="col-12 col-md-9">
-                      <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                      <Form model = "feedback" onSubmit={(values) => this.handleSubmit(values)}>
                           <Row className = "form-group">
                           <Label htmlFor="firstname" md={2}>First Name</Label>
                           <Col md={10}>
@@ -216,7 +217,7 @@ and the validate also not require*/
                                 </Button>
                             </Col>
                           </Row>
-                     </LocalForm> 
+                     </Form> 
                    </div>
                </div>
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,7 @@ import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators'
-
+import { actions } from 'react-redux-form';
 /* mapDispatchToProps is a function that will return and it will take 4 params and it will dispatch
 the addComment action creator needs to be passed these 4 params values*/
 
@@ -24,7 +24,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({ 
    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-   fetchDishes: () => {dispatch(fetchDishes())}
+   fetchDishes: () => {dispatch(fetchDishes())},
+   resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 });
 
 
@@ -73,7 +74,7 @@ class Main extends Component {
            <Route path="/home" component={HomePage} />
            <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
            <Route path="/menu/:dishId" component={DishWithId} />
-           <Route exact path="/contactus" component={Contact} />
+           <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
            <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
            <Redirect to="/home" />
          </Switch>
